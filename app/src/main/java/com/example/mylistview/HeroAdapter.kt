edd3347598bd45af4fe215de644f131094f093b4
faridl29 <m.faridl441@gmail.com>
2020-05-27 07:04:46 +0700

    first commit

---
+++ app/src/main/java/com/example/mylistview/HeroAdapter.kt
@@ -0,0 +1,49 @@
+package com.example.mylistview
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import de.hdodenhof.circleimageview.CircleImageView
+import kotlinx.android.synthetic.main.item_hero.view.*
+
+class HeroAdapter internal constructor(private val context:Context) : BaseAdapter(){
+    internal var heroes = arrayListOf<Hero>()
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        var itemView = convertView
+        if(itemView == null){
+            itemView = LayoutInflater.from(context).inflate(R.layout.item_hero, parent, false)
+        }
+
+        val viewHolder = ViewHolder(itemView as View)
+
+        val hero = getItem(position) as Hero
+        viewHolder.bind(hero)
+        return itemView
+    }
+
+    override fun getItem(position: Int): Any {
+        return heroes[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getCount(): Int {
+        return heroes.size
+    }
+
+    private inner class ViewHolder internal constructor(private val view: View){
+        internal fun bind(hero: Hero) {
+            with(view){
+                txt_name.text = hero.name
+                txt_description.text = hero.description
+                img_photo.setImageResource(hero.photo)
+            }
+        }
+    }
+}
